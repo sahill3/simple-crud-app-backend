7ab135d3a6cb2e2ec400bb941341a7a37f4e0dfe
sahill3 <sahil.mandavkar3@gmail.com>
2024-02-29 11:33:58 +0530

    Initial Commit

---
+++ routes/product.route.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/product.model.js");
+const {getProducts, getSingleProduct, createProduct, updateProduct, deleteProduct} = require("../controllers/product.controller.js");
+
+router.get("/", getProducts);
+router.get("/:id", getSingleProduct);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+module.exports = router; 
